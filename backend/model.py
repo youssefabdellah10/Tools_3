a778a7eb5cba650e095248a4786da1c3ebb3e2a3
youssef abdellah <youssefabdellah10@outlook.com>
2024-10-31 22:21:51 +0300

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -8,7 +8,7 @@ class UserModel(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     phone = db.Column(db.String(100), unique = False, nullable=False)
     password = db.Column(db.String(256), unique =False, nullable=False)
-    orders = db.relationship('OrderModel', back_populates='user')
+    orders = db.relationship('OrderModel', backref='user',lazy='dynamic')
     
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -52,13 +52,12 @@ class CourierModel(db.Model):
     name = db.Column(db.String(100), unique = False, nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(256), unique =False, nullable=False)
-    orders = db.relationship('OrderModel', back_populates='courier')
+    orders = db.relationship('OrderModel', backref='couriers',lazy='dynamic')
     
     def set_password(self, password):
         self.password = generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password, password)
-    
     
 
 class ProductModel(db.Model):
@@ -67,7 +66,7 @@ class ProductModel(db.Model):
     name = db.Column(db.String(100), unique = False, nullable=False)
     price = db.Column(db.Float, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
-    order_associations = db.relationship('OrderProducts', back_populates='product')
+    order = db.relationship('orders', secondary='orders_products', back_populates='products')
     
     def json(self):
         return {"name": self.name,
@@ -75,15 +74,17 @@ class ProductModel(db.Model):
                 "quantity": self.quantity
                 }
         
+        
 class OrderModel(db.Model):
     table_name = 'orders'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     total = db.Column(db.Float, nullable=False)
     status = db.Column(db.String(100), nullable=False)
-    product = db.relationship('ProductModel', back_populates='order_associations')
-    order = db.relationship('OrderModel', back_populates='product_associations')
-    courier = db.relationship('CourierModel', back_populates='orders')
-    user = db.relationship('UserModel', back_populates='orders')
+    products = db.relationship('ProductModel', secondary='orders_products', back_populates='orders')
+    courier_id = db.Column(db.Integer, db.ForeignKey(CourierModel.id))
+    courier_order = db.relationship('CourierModel', back_populates='orders')
+    user_id = db.Column(db.Integer, db.ForeignKey(UserModel.id))
+    user_order= db.relationship('UserModel', back_populates='orders')
     
     def json(self):
         return {"id": self.id,
@@ -93,5 +94,10 @@ class OrderModel(db.Model):
                 "courier_id": self.courier_id,
                 "list_of_products": [product.json() for product in self.products]
                 }
+        
+order_product = db.Table('orders_products',
+    db.Column('order_id', db.Integer, db.ForeignKey(OrderModel.id), primary_key=True),
+    db.Column('product_id', db.Integer, db.ForeignKey(ProductModel.id), primary_key=True)
+)
     
         
