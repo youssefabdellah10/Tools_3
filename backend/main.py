5cf1ed9c8893a7a58d52db111eeff8d69ab6c0b2
youssef abdellah <youssefabdellah10@outlook.com>
2024-10-31 23:38:03 +0200

    update
    fix the model and create endpoint for add product

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify
 from config import app, db
-from model import UserModel
+from model import UserModel , AdminModel, CourierModel, OrderModel, ProductModel
 
 @app.route('/users/signup', methods=['POST'])
 def signup():
@@ -26,40 +26,21 @@ def login():
         return jsonify(user.json()), 200
     return jsonify({'message': 'password'}), 401  # Change to 401 for wrong password
 
-""""
-@app.route('/products/create', methods=['POST'])
-def create_product():
+@app.route('/products/addproduct', methods=['POST'])
+def products():
     data = request.get_json()
     new_product = ProductModel(name=data['name'], price=data['price'], quantity=data['quantity'])
+    if new_product.quantity < 0 : 
+        return jsonify({'message': 'Quantity cannot be negative'}), 400
+    if  not new_product.price or not new_product.quantity: 
+        return jsonify({'error'}), 400
     try:
         db.session.add(new_product)
         db.session.commit()
     except Exception as e:
         return jsonify({'message': str(e)}), 400
     return jsonify(new_product.json()), 201
-
-@app.route('/orders/create', methods=['POST'])
-def create_order():
-    data = request.get_json()
-    total_price = 0
-    try:
-        for product_data in data['products']:
-            product = ProductModel.query.get(product_data['product_id'])
-            if product and product.quantity >= product_data['quantity']:
-                total_price += product.price * product_data['quantity']
-                product.quantity -= product_data['quantity']
-                db.session.execute(order_products.insert().values(order_id=new_order.id, product_id=product.id, quantity=product_data['quantity']))
-            else:
-                raise Exception('Product not found or insufficient quantity')
-        new_order = OrderModel(user_id=data['user_id'],total=total_price, status='pending')
-        db.session.add(new_order)
-        db.session.flush()
-        db.session.commit()
-    except Exception as e:
-        db.rollback()
-        return jsonify({'message': str(e)}), 400
-    return jsonify(new_order.json()), 201
-"""
+    
 
 if __name__ == '__main__':
     with app.app_context():
--- backend/model.py
+++ backend/model.py
@@ -8,7 +8,7 @@ class UserModel(db.Model):
     email = db.Column(db.String(100), nullable=False, unique=True)
     phone = db.Column(db.String(100), unique = False, nullable=False)
     password = db.Column(db.String(256), unique =False, nullable=False)
-    orders = db.relationship('OrderModel', backref='user',lazy='dynamic')
+    orders = db.relationship('OrderModel', back_populates='user',lazy='dynamic')
     
     def set_password(self, password):
         self.password = generate_password_hash(password)
@@ -21,7 +21,7 @@ class UserModel(db.Model):
         return {"name": self.name,
                 "email": self.email,
                 "phone": self.phone
-                }
+        }
         
         
         
@@ -44,47 +44,32 @@ class AdminModel(db.Model):
         return {"name": self.name,
                 "email": self.email,
                 "phone": self.phone
-                }
+        }
 
 class CourierModel(db.Model):
-    table_name = 'couriers'
+    __tablename__ = 'couriers'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), unique = False, nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(256), unique =False, nullable=False)
-    orders = db.relationship('OrderModel', backref='couriers',lazy='dynamic')
+    orders = db.relationship('OrderModel', back_populates='courier',lazy='dynamic')
     
     def set_password(self, password):
         self.password = generate_password_hash(password)
     def check_password(self, password):
         return check_password_hash(self.password, password)
     
-
-class ProductModel(db.Model):
-    table_name = 'products'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), unique = False, nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-    order = db.relationship('orders', secondary='orders_products', back_populates='products')
     
-    def json(self):
-        return {"name": self.name,
-                "price": self.price,
-                "quantity": self.quantity
-                }
-        
-        
 class OrderModel(db.Model):
-    table_name = 'orders'
+    __tablename__ = 'orders'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     total = db.Column(db.Float, nullable=False)
     status = db.Column(db.String(100), nullable=False)
     products = db.relationship('ProductModel', secondary='orders_products', back_populates='orders')
-    courier_id = db.Column(db.Integer, db.ForeignKey(CourierModel.id))
-    courier_order = db.relationship('CourierModel', back_populates='orders')
-    user_id = db.Column(db.Integer, db.ForeignKey(UserModel.id))
-    user_order= db.relationship('UserModel', back_populates='orders')
+    courier_id = db.Column(db.Integer, db.ForeignKey('couriers.id'),index = True)
+    courier = db.relationship('CourierModel', back_populates='orders')
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'),index = True)
+    user= db.relationship('UserModel', back_populates='orders')
     
     def json(self):
         return {"id": self.id,
@@ -93,11 +78,26 @@ class OrderModel(db.Model):
                 "user_id": self.user_id,
                 "courier_id": self.courier_id,
                 "list_of_products": [product.json() for product in self.products]
+        }
+     
+
+class ProductModel(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), unique = False, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    orders = db.relationship('OrderModel', secondary='orders_products', back_populates='products')
+    
+    def json(self):
+        return {"name": self.name,
+                "price": self.price,
+                "quantity": self.quantity
                 }
-        
+           
 order_product = db.Table('orders_products',
-    db.Column('order_id', db.Integer, db.ForeignKey(OrderModel.id), primary_key=True),
-    db.Column('product_id', db.Integer, db.ForeignKey(ProductModel.id), primary_key=True)
+    db.Column('order_id', db.Integer, db.ForeignKey('orders.id')),
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'))
 )
     
         
