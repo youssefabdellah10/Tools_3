24ad44ddcfc56fa508ac6f5dc8a414c9dd7401d5
malak-y <145293320+malak-y@users.noreply.github.com>
2024-11-04 08:18:57 +0200

    update

--- backend/main.py
+++ backend/main.py
@@ -152,7 +152,46 @@ def delete_order():
     return jsonify({'message': 'Order is deleted'}), 200
 
 
-    
+    #Couruier features
+
+#get Courier orders
+@app.route('/CourieOrder', methods=['GET'])
+def get_Courier_orders():
+    orders = OrderModel.query.all()
+    courier_id = request.args.get('courier_id',type=int)
+    if not courier_id:
+        return jsonify({'message': 'Bad request , Please enter a correct ID for the courier'}), 400
+    courier = db.session.get(CourierModel,courier_id)
+    if not courier:
+        return jsonify({'message': 'Courier ID not found'}), 404
+    assigned_Order = []
+    for order in orders:
+        if order.courier is courier:
+         assigned_Order.append(order)
+    if not assigned_Order:
+        return  jsonify({'message': 'No assigned orders'}), 200  
+    return jsonify([order.json() for order in assigned_Order]), 200
+
+# Accept Order
+@app.route('/acceptOrder', methods=['PUT'])
+def accept_order():
+    data = request.get_json() 
+    order_id = data.get('order_id')
+    order = OrderModel.query.get(order_id)
+    order.status = 'picked up'
+    db.session.commit()
+    return jsonify({'message': 'Order is accepted successfully'}),200
+
+# Decline Order
+@app.route('/DeclineOrder', methods=['PUT'])
+def decline_order():
+    data = request.get_json() 
+    order_id = data.get('order_id')
+    order = OrderModel.query.get(order_id)
+    order.courier = None
+    db.session.commit()
+    return jsonify({'message': 'Order is declined successfully'}),200
+
     
 
 if __name__ == '__main__':
