86db7388ac5af42a58c510dad787c43215e2734d
youssef abdellah <youssefabdellah10@outlook.com>
2024-12-24 13:08:33 +0200

    234

--- backend/config.py
+++ backend/config.py
@@ -4,9 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
-
+CORS(app, resources={r"/*": {"origins": "https://tools-3-git-youssefabdellah10-dev.apps.rm3.7wse.p1.openshiftapps.com"}})
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:1234@localhost:5432/store')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
